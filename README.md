85acdc9b52e9f1a0608f5ff9e0f782307da08e9a
Polina Tutygina <polina.tutygina@yandex.ru>
2025-01-07 12:21:07 +0300

    The finished project.

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ cd kittygram2plus
 Cоздать и активировать виртуальное окружение:
 
 ```
-python3 -m venv env
+python -m venv env
 ```
 
 ```
@@ -21,7 +21,7 @@ source env/bin/activate
 ```
 
 ```
-python3 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 ```
 
 Установить зависимости из файла requirements.txt:
@@ -33,11 +33,11 @@ pip install -r requirements.txt
 Выполнить миграции:
 
 ```
-python3 manage.py migrate
+python manage.py migrate
 ```
 
 Запустить проект:
 
 ```
-python3 manage.py runserver
+python manage.py runserver
 ```
--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,29 @@
 from rest_framework import viewsets
 
+from rest_framework import permissions
+
+from .permissions import OwnerOrReadOnly, ReadOnly
+
 from .models import Achievement, Cat, User
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import filters
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+    permission_classes = (OwnerOrReadOnly,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
+    pagination_class = None
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',) 
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
